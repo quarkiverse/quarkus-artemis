94903d7b41221126659050f270349c2385836b26
Zheng Feng <zh.feng@gmail.com>
2022-06-15 00:21:40 +0800

    Remove transaction manager in wrapper

--- jms/runtime/src/main/java/io/quarkus/artemis/jms/runtime/ArtemisJmsWrapper.java
+++ jms/runtime/src/main/java/io/quarkus/artemis/jms/runtime/ArtemisJmsWrapper.java
@@ -1,10 +1,9 @@
 package io.quarkus.artemis.jms.runtime;
 
 import javax.jms.ConnectionFactory;
-import javax.transaction.TransactionManager;
 
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 
 public interface ArtemisJmsWrapper {
-    ConnectionFactory wrapConnectionFactory(ActiveMQConnectionFactory cf, TransactionManager tm);
+    ConnectionFactory wrapConnectionFactory(ActiveMQConnectionFactory cf);
 }
