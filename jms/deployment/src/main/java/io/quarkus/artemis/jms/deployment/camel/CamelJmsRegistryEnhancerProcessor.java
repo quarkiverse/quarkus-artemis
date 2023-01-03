77bb9a2d9d47dd0e0b8937fd1a9e667ed840b82c
Marco Bungart <marco.bungart@googlemail.com>
2023-01-03 14:55:24 +0100

    Added possibility to explicitly enalbe the CamelContextEnhancer, and disabled it by default

--- jms/deployment/src/main/java/io/quarkus/artemis/jms/deployment/camel/CamelJmsRegistryEnhancerProcessor.java
+++ jms/deployment/src/main/java/io/quarkus/artemis/jms/deployment/camel/CamelJmsRegistryEnhancerProcessor.java
@@ -3,6 +3,7 @@ package io.quarkus.artemis.jms.deployment.camel;
 import org.apache.camel.quarkus.core.CamelCapabilities;
 
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.artemis.jms.deployment.ArtemisJmsBuildTimeConfig;
 import io.quarkus.artemis.jms.runtime.camel.CamelContextEnhancer;
 import io.quarkus.deployment.Capabilities;
 import io.quarkus.deployment.annotations.BuildStep;
@@ -10,8 +11,10 @@ import io.quarkus.deployment.annotations.BuildStep;
 public class CamelJmsRegistryEnhancerProcessor {
     @SuppressWarnings("unused")
     @BuildStep
-    AdditionalBeanBuildItem addCamelContextEnhancer(Capabilities capabilities) {
-        if (capabilities.isPresent(CamelCapabilities.CORE)) {
+    AdditionalBeanBuildItem addCamelContextEnhancer(Capabilities capabilities,
+            ArtemisJmsBuildTimeConfig artemisJmsBuildTimeConfig) {
+        if (artemisJmsBuildTimeConfig.isCamelQuarkusEnhanceEnable()
+                && capabilities.isPresent(CamelCapabilities.CORE)) {
             return AdditionalBeanBuildItem.unremovableOf(CamelContextEnhancer.class);
         }
         return null;
