25416e91a1eea41feef1ac5b3c6100ac60e6ea90
Marco Bungart <marco.bungart@googlemail.com>
2023-11-03 09:48:33 +0100

    When no message can be received, return null
    (cherry picked from commit c7a6ac8cc2de06c07bc9af64a776ea591fe2a9b7)

--- integration-tests/jms/common/src/main/java/io/quarkus/it/artemis/jms/common/ArtemisJmsConsumerManager.java
+++ integration-tests/jms/common/src/main/java/io/quarkus/it/artemis/jms/common/ArtemisJmsConsumerManager.java
@@ -1,9 +1,12 @@
 package io.quarkus.it.artemis.jms.common;
 
+import java.util.Optional;
+
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSConsumer;
 import javax.jms.JMSContext;
 import javax.jms.JMSException;
+import javax.jms.Message;
 
 public class ArtemisJmsConsumerManager {
     private final ConnectionFactory connectionFactory;
@@ -17,8 +20,13 @@ public class ArtemisJmsConsumerManager {
     public String receive() {
         try (JMSContext context = connectionFactory.createContext(JMSContext.AUTO_ACKNOWLEDGE);
                 JMSConsumer consumer = context.createConsumer(context.createQueue(queueName))) {
-            return consumer.receive(1000L).getBody(String.class);
-        } catch (JMSException | NullPointerException e) {
+            Optional<Message> maybeMessage = Optional.ofNullable(consumer.receive(1000L));
+            if (maybeMessage.isPresent()) {
+                return maybeMessage.get().getBody(String.class);
+            } else {
+                return null;
+            }
+        } catch (JMSException e) {
             throw new RuntimeException("Could not receive message", e);
         }
     }
