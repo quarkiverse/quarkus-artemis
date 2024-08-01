2faa2fe1bb41fae505f7f6ed176907cb29214948
Marco Bungart <marco.bungart@googlemail.com>
2024-08-01 23:38:09 +0200

    Simplify health checks

--- core/runtime/src/main/java/io/quarkus/artemis/core/runtime/health/ArtemisHealthSupport.java
+++ core/runtime/src/main/java/io/quarkus/artemis/core/runtime/health/ArtemisHealthSupport.java
@@ -4,18 +4,12 @@ import java.util.Set;
 
 public class ArtemisHealthSupport {
     private final Set<String> configuredNames;
-    private final Set<String> excludedNames;
 
-    public ArtemisHealthSupport(Set<String> configuredNames, Set<String> excludedNames) {
+    public ArtemisHealthSupport(Set<String> configuredNames) {
         this.configuredNames = configuredNames;
-        this.excludedNames = excludedNames;
     }
 
     public Set<String> getConfiguredNames() {
         return configuredNames;
-    }
-
-    public Set<String> getExcludedNames() {
-        return excludedNames;
     }
 }
