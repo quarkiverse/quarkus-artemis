9d2253667c4a80be024031ba785d8af957edf082
Alex Kovalenko <69167029+alex-kovalenko1982@users.noreply.github.com>
2025-11-05 15:38:29 +0100

    Fix import formatting in EmbeddedHealthCheckTest

--- integration-tests/core/with-default/src/test/java/io/quarkus/it/artemis/core/withdefault/embedded/EmbeddedHealthCheckTest.java
+++ integration-tests/core/with-default/src/test/java/io/quarkus/it/artemis/core/withdefault/embedded/EmbeddedHealthCheckTest.java
@@ -1,8 +1,8 @@
 package io.quarkus.it.artemis.core.withdefault.embedded;
 
 import io.quarkus.artemis.test.ArtemisTestResource;
-import io.quarkus   .it.artemis.core.withdefault.BaseArtemisHealthCheckTest;
-                import io.quarkus.test.common.WithTestResource;
+import io.quarkus.it.artemis.core.withdefault.BaseArtemisHealthCheckTest;
+import io.quarkus.test.common.WithTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
