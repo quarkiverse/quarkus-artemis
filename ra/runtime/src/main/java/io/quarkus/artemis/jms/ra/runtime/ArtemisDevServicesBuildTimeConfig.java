ccb700289b179dff32bd34816f9961e4a383c126
Marco Bungart <marco.bungart@googlemail.com>
2026-01-26 19:10:42 +0100

    Update quay.io/arkmq-org/activemq-artemis-broker from artemis.2.44.0 to artemis.2.50.0

--- ra/runtime/src/main/java/io/quarkus/artemis/jms/ra/runtime/ArtemisDevServicesBuildTimeConfig.java
+++ ra/runtime/src/main/java/io/quarkus/artemis/jms/ra/runtime/ArtemisDevServicesBuildTimeConfig.java
@@ -32,7 +32,7 @@ public interface ArtemisDevServicesBuildTimeConfig {
     /**
      * The ActiveMQ Artemis container image to use.
      * <p>
-     * Defaults to {@code quay.io/arkmq-org/activemq-artemis-broker:artemis.2.44.0}
+     * Defaults to {@code quay.io/arkmq-org/activemq-artemis-broker:artemis.2.50.0}
      */
     Optional<String> imageName();
 
